2ddef1ffa08f74eadb0390ab2ed45e94d1f5390d
earthbound1984 <91579482+earthbound1984@users.noreply.github.com>
2022-05-03 19:20:22 +0800

    Add files via upload

---
+++ 素数.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	int a,i, flag1, flag2;
+	long sum;
+	a = 2; flag1 = 0; flag2 = 0; sum = 0;
+	for (i = 100; i < 1000; i++)
+	{
+		flag1 = 0;                //important!
+		while (a < i)
+		{
+			if (i % a == 0)
+				flag1 = 1;
+			a++;
+		}
+		a = 2;                   //important!
+		if (flag1 == 0)
+		{
+			printf("%d ", i);
+			flag2++;
+			sum = sum + i;
+		}
+		if (flag2 == 5)
+		{
+			printf("\n");
+			flag2 = 0;
+		}
+	}
+	printf("\n素数之和为%ld", sum);
+}
+
